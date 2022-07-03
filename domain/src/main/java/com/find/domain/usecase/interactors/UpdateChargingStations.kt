f9cef8667c06e8959e5c7beff6bb0f3f6e780462
Juan Pablo Scigliano <juanscigliano@gmail.com>
2022-07-03 21:11:05 +0200

    [Domain Module] Inject AppDispatcher to interactors

--- domain/src/main/java/com/find/domain/usecase/interactors/UpdateChargingStations.kt
+++ domain/src/main/java/com/find/domain/usecase/interactors/UpdateChargingStations.kt
@@ -1,33 +1,32 @@
 package com.find.domain.usecase.interactors
 
+import com.find.domain.AppDispatchers
 import com.find.domain.interactor.Interactor
 import com.find.domain.model.Distance
 import com.find.domain.model.DistanceUnit
 import com.find.domain.repository.ChargingStationRepository
 import com.find.domain.repository.LocationRepository
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 
 class UpdateChargingStations @Inject constructor(
     private val chargingStationsRepository: ChargingStationRepository,
-    private val locationRepository: LocationRepository
+    private val locationRepository: LocationRepository,
+    private val appDispatchers: AppDispatchers
 ) : Interactor<UpdateChargingStations.Params>() {
     override suspend fun execute(params: Params) {
-
-        locationRepository.getLatestUserLocation()?.let { coordinates ->
-            withContext(Dispatchers.IO) {
+        withContext(appDispatchers.io) {
+            locationRepository.getLatestUserLocation()?.let {
                 chargingStationsRepository.updatePOIClosedToCoordinates(
-                    coordinates,
-                    params.distanceFromCoordinates
+                    coordinates = it,
+                    distanceFromCoordinate = params.distanceFromCoordinates
                 )
             }
         }
     }
 
     data class Params(
-        // val coordinates: Coordinates,
         val distanceFromCoordinates: Distance,
         val distanceUnit: DistanceUnit
     )
--- domain/src/main/java/com/find/domain/usecase/interactors/UpdateUserLocation.kt
+++ domain/src/main/java/com/find/domain/usecase/interactors/UpdateUserLocation.kt
@@ -1,25 +1,23 @@
 package com.find.domain.usecase.interactors
 
 
+import com.find.domain.AppDispatchers
 import com.find.domain.interactor.Interactor
 import com.find.domain.model.Coordinates
 import com.find.domain.model.Latitude
 import com.find.domain.model.Longitude
 import com.find.domain.repository.LocationRepository
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 
 class UpdateUserLocation @Inject constructor(
-    private val locationRepository: LocationRepository
+    private val locationRepository: LocationRepository,
+    private val appDispatchers: AppDispatchers
 ) : Interactor<Unit>() {
-    override suspend fun execute(params: Unit) {
-
+    override suspend fun execute(params: Unit) = withContext(appDispatchers.io) {
         locationRepository.saveUserLocation(
-            Coordinates(
-                Latitude(52.526),
-                Longitude(13.415)
-            )
+            coordinates = Coordinates(Latitude(52.526), Longitude(13.415))
         )
-
     }
 }
